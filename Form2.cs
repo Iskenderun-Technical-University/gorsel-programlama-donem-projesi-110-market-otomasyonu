604c8d11d1b59c62312e8fa4c95d483c3786819f
Metin Çelik <metincelik.mdbf21@iste.edu.tr>
2022-10-18 21:18:04 +0300

    Hata düzeltildi.

--- Form2.cs
+++ Form2.cs
@@ -37,5 +37,13 @@ namespace Market_Otomasyonu
             maashesaplamasec.Show();
             this.Close();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Stuff stuff = new Stuff();
+            stuff.Show();
+            this.Hide();
+
+        }
     }
 }
