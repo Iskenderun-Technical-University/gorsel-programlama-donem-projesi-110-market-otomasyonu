4e9d0b1890ec51e5fc4442e8a145ae86e5244621
Metin Çelik <metincelik.mdbf21@iste.edu.tr>
2022-10-27 15:04:06 +0300

    Sipariş Verme Formu Eklendi.

--- OrderForm.Designer.cs
+++ OrderForm.Designer.cs
@@ -28,15 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
-            "Deneme",
-            "5",
-            "6"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem(new string[] {
+            "512356",
+            "Coca Cola 1L",
+            "12,5",
+            "38"}, -1);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OrderForm));
             this.listView1 = new System.Windows.Forms.ListView();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -48,9 +48,10 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.groupBox1.SuspendLayout();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // listView1
@@ -58,7 +59,7 @@
             this.listView1.Font = new System.Drawing.Font("Open Sans", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.listView1.HideSelection = false;
             this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem2});
+            listViewItem4});
             this.listView1.Location = new System.Drawing.Point(6, 30);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(670, 379);
@@ -69,7 +70,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Open Sans", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(510, 25);
+            this.label2.Location = new System.Drawing.Point(510, 28);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(179, 38);
             this.label2.TabIndex = 2;
@@ -78,8 +79,6 @@
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
-            this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.groupBox1.Font = new System.Drawing.Font("Open Sans", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.groupBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
@@ -89,28 +88,6 @@
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Sepetim";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Open Sans", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label6.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.label6.Location = new System.Drawing.Point(150, 44);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(44, 20);
-            this.label6.TabIndex = 1;
-            this.label6.Text = "Adet";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Open Sans", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label5.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.label5.Location = new System.Drawing.Point(30, 44);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(47, 20);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "Ürün";
             // 
             // groupBox2
             // 
@@ -148,6 +125,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Temizle";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // groupBox3
             // 
@@ -224,6 +202,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Tamamla";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
@@ -236,11 +215,22 @@
             this.label1.TabIndex = 5;
             this.label1.Text = "İsim:";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(460, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(44, 35);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
             // OrderForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1141, 723);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label2);
@@ -248,11 +238,10 @@
             this.Name = "OrderForm";
             this.Text = "OrderForm";
             this.Load += new System.EventHandler(this.OrderForm_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -274,7 +263,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- OrderForm.cs
+++ OrderForm.cs
@@ -29,6 +29,31 @@ namespace Market_Otomasyonu
 
             for (int i = 0; i < x; i++) {
 
+                // Label Urun Adı Başlık
+                System.Windows.Forms.Label label5 = new System.Windows.Forms.Label();
+                label5.AutoSize = true;
+                label5.Font = new System.Drawing.Font("Open Sans", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+                label5.ForeColor = System.Drawing.SystemColors.InfoText;
+                label5.Location = new System.Drawing.Point(20, 38);
+                label5.Name = "label5";
+                label5.Size = new System.Drawing.Size(47, 20);
+                label5.TabIndex = 0;
+                label5.Text = "Ürün";
+
+                groupBox1.Controls.Add(label5);
+
+                // Label Adet Başlık
+                System.Windows.Forms.Label label6 = new System.Windows.Forms.Label();
+                label6.AutoSize = true;
+                label6.Font = new System.Drawing.Font("Open Sans", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+                label6.ForeColor = System.Drawing.SystemColors.InfoText;
+                label6.Location = new System.Drawing.Point(110, 38);
+                label6.Name = "label5";
+                label6.Size = new System.Drawing.Size(47, 20);
+                label6.TabIndex = 0;
+                label6.Text = "Adet";
+
+                groupBox1.Controls.Add(label6);
 
                 //Label urunadi
                 System.Windows.Forms.Label nlabel = new System.Windows.Forms.Label();
@@ -39,7 +64,7 @@ namespace Market_Otomasyonu
                 nlabel.Name = "label"+y.ToString();
                 nlabel.Size = new System.Drawing.Size(57, 20);
                 nlabel.TabIndex = 0;
-                nlabel.Text = "Ürün" + (y-5).ToString();
+                nlabel.Text = listView1.Items[0].SubItems[1].Text;
                 
                 groupBox1.Controls.Add(nlabel);
 
@@ -91,10 +116,30 @@ namespace Market_Otomasyonu
         
         }
 
+        public void RemoveSelections()
+        {
+            groupBox1.Controls.Clear();
+            // Label sepette ürün yok
+            System.Windows.Forms.Label nlabel3 = new System.Windows.Forms.Label();
+            nlabel3.AutoSize = true;
+            nlabel3.Font = new System.Drawing.Font("Open Sans", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            nlabel3.ForeColor = System.Drawing.SystemColors.MenuText;
+            nlabel3.Location = new System.Drawing.Point(12, 39);
+            nlabel3.Name = "labelempty";
+            nlabel3.Size = new System.Drawing.Size(158, 20);
+            nlabel3.TabIndex = 0;
+            nlabel3.Text = "Sepetinizde ürün yok.";
+
+            groupBox1.Controls.Add(nlabel3);
+
+            listView1.Items[0].Checked = false;
+        }
 
         private void OrderForm_Load(object sender, EventArgs e)
         {
+            RemoveSelections();
             
+            // ListView Configuration
             listView1.GridLines = true;
             listView1.FullRowSelect = true;
             listView1.View = View.Details;
@@ -110,13 +155,48 @@ namespace Market_Otomasyonu
 
         private void button3_Click(object sender, EventArgs e)
         {
-            int x = 10;
-            int y = 6;
-            int z = 70;
-            int t = 65;
-            int p = 70;
+            if (listView1.CheckedItems.Count!=0)
+            {
+                groupBox1.Controls.Clear();
+                int x = listView1.CheckedItems.Count;
+                int y = 7;
+                int z = 70;
+                int t = 65;
+                int p = 70;
 
-            this.createRow(x, y, z, t, p);
+                this.createRow(x, y, z, t, p);
+            }
+            else
+            {
+                MessageBox.Show("Lütfen en az bir ürün seçin.", "Eksik Ürün", MessageBoxButtons.OK, icon: MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RemoveSelections();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (groupBox1.Controls.Count < 3)
+            {
+                MessageBox.Show("Lütfen sepetinize ürün ekleyin.", "Sepetiniz Boş", MessageBoxButtons.OK, icon: MessageBoxIcon.Warning);
+            }
+            else if(textBox1.Text=="" || textBox2.Text== "" || textBox2.Text == "")
+            {
+                MessageBox.Show("Lütfen sipariş bilgilerinizi eksiksiz girin.", "Eksik Bilgi", MessageBoxButtons.OK, icon: MessageBoxIcon.Warning);
+
+            }
+            else
+            {
+                MessageBox.Show("Siparişiniz başarı ile tamamlandı.", "Sipariş Alındı", MessageBoxButtons.OK, icon: MessageBoxIcon.Information);
+                RemoveSelections();
+                textBox1.Clear();   
+                textBox2.Clear();   
+                textBox3.Clear(); 
+            }
+
         }
     }
 }
