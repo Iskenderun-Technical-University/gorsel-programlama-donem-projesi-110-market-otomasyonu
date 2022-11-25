e780faf535c39f643f73cbb6142f8c7dadc78d59
Hilalyilmaz1 <hilal.yilmaz.mdbf22@iste.edu.tr>
2022-11-25 22:07:37 +0300

    Ürün ekleme yapıldı.

---
+++ Classes1/Sqlconnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Market_Otomasyonu.Classes1
+{
+   public class Sqlconnection
+    {
+        public static SqlConnection connection = new SqlConnection("Data Source=LAPTOP-HTJQEGE6;Initial Catalog=deneme;Integrated Security=True");
+
+        public static void CheckConnection(SqlConnection tempConnection)
+        {
+            if (tempConnection.State == ConnectionState.Closed)
+            {
+                tempConnection.Open();
+
+            }
+        }
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Market_Otomasyonu
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+        public void LoadCategories()
+        {
+
+            SqlCommand commandListCategories = new SqlCommand("Select * Table_2", SqlConnection.connect);
+            SqlConnection.CheckConnection(SqlConnection.connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(commandListCategories);
+            SqlDataAdapter da = new SqlDataAdapter(commandListCategories);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+
+        public  void Form3_Load(object sender, EventArgs e)
+        {
+            LoadCategories();
+
+        }
+    }
+}
